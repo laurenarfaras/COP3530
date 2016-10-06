9553c023837b582d53413ca943166fbdce5a3d13
laurenarfaras <laurenarfaras@users.noreply.github.com>
2016-10-05 22:08:58 -0400

    comments

--- insertionSortTest.cpp
+++ insertionSortTest.cpp
@@ -2,17 +2,20 @@
 using namespace std;
 
 int main(){
-    std::ios_base::sync_with_stdio(false); 
+    std::ios_base::sync_with_stdio(false);
+    //number of elements in array
     int n = 0;
     cin >> n;
     int A[n];
     
+    //read the elements and place in array
     for (int i = 0; i < n; i++){
         int number = 0;
         cin >> number;
         A[i] = number;
     }
     
+    //insertion sort algorithm
     for (int i = 1; i < n; i++) {
         int key = A[i];
         int j;
@@ -22,6 +25,7 @@ int main(){
         A[j+1] = key;
     }
     
+    //print sorted array
     for (int b = 0; b < n; b++) {
         cout << A[b] << "\n";
     }
